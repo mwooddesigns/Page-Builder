ed1badec96d0092d96251ef98f9ec1c82ed769f7
Michael Wood <mwood@markettraders.com>
2016-03-04 16:32:03 -0500

    Setting up basic form for login and creating file to hold user authentication script.

--- login.php
+++ login.php
@@ -3,11 +3,19 @@
 <head>
   <meta charset="UTF-8">
   <title>User Login</title>
+
+  <link rel="stylesheet" href="css/login.css">
 </head>
 <body>
 
   <div class="form-container">
-
+    <form action="auth_login.php">
+      Username:<br>
+      <input type="text" name="user_name"><br><br>
+      Password:<br>
+      <input type="text" name="user_pass"><br><br>
+      <input type="submit" value="Login">
+    </form>
   </div>
 
 </body>
