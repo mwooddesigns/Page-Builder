33535a9bd3a423d266e4d09caca7e7f4be173830
Michael Wood <mwood@markettraders.com>
2016-03-04 15:01:07 -0500

    Adding a very basic file structure and beginning to create dev template and login page.

---
+++ login.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>User Login</title>
+</head>
+<body>
+
+  <div class="form-container">
+
+  </div>
+
+</body>
+</html>
---
+++ templates/Basic-2-col.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap 101 Template</title>
+    <link rel="icon" type="image/png" href="/images/favicon-mti.png">
+    <!--Open Graph Tags for Facebook-->
+    <meta property="og:title" content=""/>
+    <meta property="og:site_name" content="Market Traders Institute"/>
+    <meta property="og:url" content=""/>
+    <meta property="og:description" content=""/>
+    <!--Content Type. Do Not Change unless relevant-->
+    <meta property="og:type" content="website"/>
+    <!--Image. Must be a minimun of 600x315px to work on mobile devices-->
+    <meta property="og:image" content=""/>
+    <!--END Open Graph Tags for Facebook-->
+
+    <!--Bootstrap-->
+    <link rel="stylesheet" href="http://aperture.markettraders.com/form/dependencies/bootstrap/dist/css/bootstrap.min.css" type="text/css">
+
+    <!--Bootstrap Form Helper-->
+    <link rel="stylesheet" href="http://aperture.markettraders.com/form/dependencies/bootstrap-form-helpers/dist/css/bootstrap-formhelpers.min.css" type="text/css">
+
+    <!-- FontAwesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+
+    <!-- Your StyleSheet -->
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+
+    <!--As Seen On-->
+    <link rel="stylesheet" type="text/css" href="http://markettraders.com/asseen/light/css/style.css">
+    <link rel="stylesheet" type="text/css" href="http://markettraders.com/asseen/light/css/responsive.css">
+
+    <!--As Seen On-->
+    <link rel="stylesheet" type="text/css" href="http://markettraders.com/asseen/opt/light/css/style.css">
+    <link rel="stylesheet" type="text/css" href="http://markettraders.com/asseen/opt/light/css/responsive.css">
+
+    <!-- Optimizely Snippet -->
+    <script src="//cdn.optimizely.com/js/324108805.js"></script>
+
+    <!-- HeatMap Snippet -->
+    <script type="text/javascript">
+      if (typeof hmtracker == 'undefined') {
+        var hmt_script = document.createElement('script'),
+          hmt_purl = encodeURIComponent(location.href).replace('.', '~');
+        hmt_script.type = "text/javascript";
+        hmt_script.src = "//heatmap.markettraders.com/?hmtrackerjs=Market Traders&uid=542df4b0735591682d6554f41cabe5c497e9dcd1&purl=" + hmt_purl;
+        document.getElementsByTagName('head')[0].appendChild(hmt_script);
+      }
+    </script>
+  </head>
+  <body>
+    <!-- Facebook SDK Script -->
+    <script>
+      window.fbAsyncInit = function () {
+        FB.init({appId: '560745344088810', xfbml: true, version: 'v2.5'});
+      };
+
+      (function (d, s, id) {
+        var js,
+          fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {
+          return;
+        }
+        js = d.createElement(s);
+        js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
+    </script>
+
+    <!-- BEGIN BODY CONTENT -->
+
+    <!-- END BODY CONTENT -->
+
+    <!--CORS > IE9 Polyfill-->
+    <script type="text/javascript" src="http://aperture.markettraders.com/form/dependencies/xdomain/dist/0.5/xdomain.js"
+    slave="http://gravity.markettraders.com/cors/proxy.html"></script>
+    <!--jQuery-->
+    <script type="text/javascript" src="http://aperture.markettraders.com/form/dependencies/jquery/jquery.min.js"></script>
+    <!--Bootstrap JS-->
+    <script type="text/javascript" src="http://aperture.markettraders.com/form/dependencies/bootstrap/dist/js/bootstrap.min.js"></script>
+    <!--Bootstrap Form Helper-->
+    <script type="text/javascript" src="http://aperture.markettraders.com/form/dependencies/bootstrap-form-helpers/dist/js/bootstrap-formhelpers.min.js"></script>
+    <!--Angular JS-->
+    <script type="text/javascript" src="http://aperture.markettraders.com/form/dependencies/angular/angular.min.js"></script>
+    <script src="http://code.angularjs.org/1.3.0/angular-sanitize.min.js"></script>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/bower-angular-translate/2.0.1/angular-translate.min.js"></script>
+    <!--Angular JS Bootstrap-->
+    <script type="text/javascript" src="http://aperture.markettraders.com/form/dependencies/angular-bootstrap/ui-bootstrap-tpls.min.js"></script>
+    <!--AnglujarJS Bootstrap Dialogs-->
+    <script type="text/javascript" src="http://aperture.markettraders.com/form/dependencies/dialogs/dialogs-default-translations.min.js"></script>
+    <script type="text/javascript" src="http://aperture.markettraders.com/form/dependencies/dialogs/dialogs.min.js"></script>
+    <!--ApertureFormJS-->
+    <!--<script type="text/javascript" src="http://aperture.markettraders.com/form/build/current/apertureFormJS.min.js"></script>-->
+    <script type="text/javascript" src="http://ume.markettraders.com/js/apertureFormJS.min.js"></script>
+  </body>
