10d2e32e9569154a50b41790431caaab5b3b66e7
Michael Wood <mwood@markettraders.com>
2016-03-22 10:15:36 -0400

    Fresh database dump and moving to private MTI repo for security reasons. This version might be update or repurposed later.

--- inc/connect.php
+++ inc/connect.php
@@ -4,6 +4,7 @@ ini_set('display_startup_errors',1);
 ini_set('display_errors',1);
 error_reporting(-1);
 
+// Localhost login information
 $servername = "127.0.0.1";
 $username = "root";
 $password = "root";
